714c7dd0c0f2a59de1449a987a020b5a97c0ddfc
geyao2015 <geyao2013@outlook.com>
2015-02-03 21:57:30 +0800

    完成了表单美化剩下的部分

--- 表单美化/js/javascript.js
+++ 表单美化/js/javascript.js
@@ -15,23 +15,63 @@ function show(index) {
 }
 
 //复选按钮美化
-function checklist(input) {
-    //alert(1);
-    //点击变换效果
-    //var li;
-    //li = document.getElementById("checkList").getElementsByTagName("li");
-    //
-    //for (var i = 0; li.length; i++) {
-    //    li[i].onclick = function () {
-    //    }
-        if (input.className == "selected") {
-            input.className = null;
-        } else {
-            input.className = "selected";
-        }
-    //}
+function checklist(index) {
+    var li = document.getElementById("checkList").getElementsByTagName("li");
+
+    if (li[index].className == "selected") {
+        li[index].className = null;
+    } else {
+        li[index].className = "selected";
+    }
 }
-//window.onload = checklist();
+
+//下拉菜单美化
+function addLoadEvent(func) {
+    //实现多个加载函数
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        //如果未设置，则将func直接设为启动函数
+        window.onload = func;
+    } else {
+        //否则先执行原来的启动函数，再执行func
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+function $(id) {
+    return document.getElementById(id);
+}
+function showProvince() {
+    $("selectProvince").onclick = showAllProvince;
+    selectProvince();
+}
+function showAllProvince() {
+    $("allProvince").style.display = "block";//显示下拉框
+    $("selectProvince").style.color = "#CCCCCC";//改变字体颜色
+    $("selectProvince").style.backgroundPosition = "189px -17px";//改变下拉框的小箭头图标方向
+}
+function hideAllProvince() {
+    $("allProvince").style.display = "none";
+    $("selectProvince").style.color = "#000000";
+    $("selectProvince").style.backgroundPosition = "189px -2px";
+}
+function selectProvince() {
+    var pro = $("allProvince").getElementsByTagName("li");
+    var links;
+    for (var i = 0; i < pro.length; i++) {
+        links = pro[i].getElementsByTagName("a");
+        for (var j = 0; j < links.length; j++) {
+            links[j].onclick = function () {
+                $("selectProvince").innerHTML = this.innerHTML;
+                hideAllProvince();
+            }
+        }
+    }
+}
+addLoadEvent(showProvince);
+
 
 
 
