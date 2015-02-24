ede595207d29acb7c656d4af80e11b197a8d8f8f
wuyue <geyao2013@outlook.com>
2015-02-24 14:58:37 +0800

    完成了tooltip浮动提示框Demo

--- tooltip浮动提示框特效/script.js
+++ tooltip浮动提示框特效/script.js
@@ -1,15 +1,49 @@
+var isIE = navigator.userAgent.indexOf("MSIE") > -1;//判别IE浏览器
+
+
 function showToolTip(obj, id, html, width, height) {
     if (document.getElementById(id) == null) {
         //创建
         var tooltipbox;
-        tooltipbox=document.createElement("div");
-        tooltipbox.className="tooltip-box";
-        tooltipbox.id=id;
-        tooltipbox.innerHTML=html;
+        tooltipbox = document.createElement("div");
+        tooltipbox.className = "tooltip-box";
+        tooltipbox.id = id;
+        tooltipbox.innerHTML = html;
         obj.appendChild(tooltipbox);
 
-        obj.onmouseleave=function(){
-            document.getElementById(id).style.display="none";
+        //设置宽高
+        tooltipbox.style.width = width ? width + "px" : "auto";
+        tooltipbox.style.height = height ? height + "px" : "auto";
+
+        if (!width && isIE) {
+            //IE浏览器不支持width的auto属性
+            tooltipbox.style.width = tooltipbox.offsetWidth;
+        }
+
+        //设置定位
+        tooltipbox.style.position = "absolute";
+        tooltipbox.style.display = "block";
+
+        var left = obj.offsetLeft;
+        var top = obj.offsetTop + 20;
+
+        //left不让tooltip提示框超出浏览器
+        if (left + tooltipbox.offsetWidth > document.body.clientWidth) {
+            var demoleft = document.getElementById("demo").offsetLeft;
+            left = document.body.clientWidth - tooltipbox.offsetWidth - demoleft;
+            if (left < 0) {
+                left = 0;
+            }
+        }
+
+        tooltipbox.style.left = left + "px";
+        tooltipbox.style.top = top + "px";
+
+        //设置动画效果
+        obj.onmouseleave = function () {
+            setTimeout(function () {
+                document.getElementById(id).style.display = "none";
+            }, 300);
         }
     } else {
         //显示
@@ -18,8 +52,8 @@ function showToolTip(obj, id, html, width, height) {
 
 }
 
-var t1=document.getElementById("tooltip1");
-t1.onmouseenter =function(){
-    showToolTip(this,"t1","提示框内容");
+var t1 = document.getElementById("tooltip1");
+t1.onmouseenter = function () {
+    showToolTip(this, "t1", "提示框内容", 200);
 
 }
