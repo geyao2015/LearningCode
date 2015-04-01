f2774ca609bbbd64f8074b7394716c834ebb976c
wuyue <geyao2013@outlook.com>
2015-04-01 16:12:15 +0800

    完成demo

--- 19-焦点图轮播/js/script.js
+++ 19-焦点图轮播/js/script.js
@@ -2,36 +2,66 @@
  * Created by 岳 on 2015/4/1.
  */
 window.onload = function () {
+    var container=document.getElementById("container");
     var next = document.getElementById("next");
-    var btns = document.getElementById("btns").getElementsByTagName("span");
+    var buttoms = document.getElementById("buttoms").getElementsByTagName("span");
     var pre = document.getElementById("pre");
     var list = document.getElementById("list");
     var index = 0;
+    var timer;//定时器自动轮播
 
     //改变index
     function changeIndex() {
         //todo:对应不上，还有bug
         for (var i = 0; i < 5; i++) {
-            if (btns[i].className == "active") {
-                btns[i].className = "";
+            if (buttoms[i].className == "active") {
+                buttoms[i].className = "";
                 break;
             }
         }
-        btns[index].className = "active";
+        buttoms[index].className = "active";
     }
 
     //轮播动画
     function animate(distance) {
+        var time = 2000;
+        var interval = 10;
+        var speed = distance / (time / interval);
 
+        var newLeft = parseInt(list.style.left) + distance;
 
-        var currentLeft = parseInt(list.style.left);
-        list.style.left = currentLeft + distance + 'px';
-        if (currentLeft <= -6000) {
-            list.style.left = "-1000px";
+        function go() {
+            if ((speed < 0 && (parseInt(list.style.left) > newLeft)) || (speed > 0 && (parseInt(list.style.left) < newLeft))) {
+                list.style.left = parseInt(list.style.left) + speed + 'px';
+                setTimeout(go, interval);
+            }
+            else {
+                list.style.left = newLeft + 'px';
+                changeIndex();
+                if (newLeft <= -6000) {
+                    list.style.left = "-1000px";
+                }
+                if (newLeft >= 0) {
+                    list.style.left = "-5000px";
+                }
+            }
         }
-        if (currentLeft >= 0) {
-            list.style.left = "-5000px";
-        }
+
+        go();
+
+    }
+
+    //自动轮播
+    function autoPlay() {
+        timer = setInterval(function () {
+            next.onclick();
+        }, 2000)
+    }
+
+    autoPlay();
+    function stopPlay() {
+        //alert(1)
+        //clearInterval(timer);
     }
 
     next.onclick = function () {
@@ -41,7 +71,7 @@ window.onload = function () {
         else {
             index++;
         }
-        changeIndex();
+        //changeIndex();
         animate(-1000);
 
     }
@@ -52,12 +82,12 @@ window.onload = function () {
         else {
             index--;
         }
-        changeIndex();
+        //changeIndex();
         animate(1000);
     }
     //绑定按钮
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].onclick = function () {
+    for (var i = 0; i < buttoms.length; i++) {
+        buttoms[i].onclick = function () {
             if (!this.className) {
                 var toIndex = this.getAttribute("index");
                 var moveDistance = (toIndex - index) * -1000;
@@ -67,5 +97,7 @@ window.onload = function () {
             }
         }
     }
+    container.onmouseover = stopPlay();
+    container.onmouseout = autoPlay();
 
 }
